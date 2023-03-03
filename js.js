e303c7fbc14650e1f0c5911573fe38edaa3f4318
Debbie <debby.ek@yahoo.com>
2023-03-03 17:37:37 -0600

    made a new script file

--- js.js
+++ js.js
@@ -29,7 +29,7 @@ const players = (score, symbol) => {
 };
 
 let player = players(0, 'X');
-let aiPlayer = players(0, 'O');
+let computer = players(0, 'O');
 
 let prompt = document.querySelector('.prompt');
 prompt.textContent = "Select a box to start!";
@@ -38,42 +38,48 @@ prompt.textContent = "Select a box to start!";
 const gameBoard = (() => {
 
 
-    let gameField = document.querySelectorAll('.gameField');
+    let gameFields = document.querySelectorAll('.gameField');
 
     let board = [];
-    for (let i = 0; i < gameField.length; i++) {
-        board.push(i);
-
+    for (let i = 0; i < gameFields.length; i++) {
+        board.push(" ");
+  
         let turn = 0;
-        
-            gameField[i].addEventListener('click', () => {
-                if(gameField[i].textContent == " "){
-                    gameField[i].innerHTML = "X";
-                    gameField[i].classList.toggle(`${player.symbol}`);
-                    board.push(player.symbol);
+
+        gameField.forEach(gameField => {
+           gameField.addEventListener('click', e => {
+                if(gameField.innerText == " "){
+                    gameField.textContent = "X";
+                    gameField.classList.toggle(`${player.symbol}`);
+                    board.push(`${player.symbol}`);
+                    prompt.textContent= " ";
                     turn++
                 }
-                game.checkWinner();
-                
             })
 
            if ( turn++) {
-                computerPlay = Math.floor(math.random()* gameField.length)
-                gameField[computerPlay].innerHTML = "O";
-                board.push(aiPlayer.symbol);
-                gameField[i].classList.toggle(`${aiPlayer.symbol}`);
-            }
-    };
-        
+                function computerPlay() {
+                let computerPick = board[Math.floor(Math.random()* gameField.length)];
+                gameField[computerPick].textContent = "O";
+                board.push(`${computer.symbol}`);
+                gameField[i].classList.toggle(`${computer.symbol}`);
+                prompt.textContent = "Player's turn";
+                };
+                return setTimeout(()=> {computerPlay}, 600)
+            };
+    }); 
+
+   
+};   
     return {board, gameField};
-    })();
+})();
 
 console.log(gameBoard.gameField);
 console.log(gameBoard.board);
  
 const game =( () => {
     player;
-    aiPlayer;
+    computer;
 
     
 
@@ -89,14 +95,16 @@ const game =( () => {
    
     ];
 
-    function checkWinner() {
-        if(gameBoard.board.indexOf(player.symbol) === winningConditions.indexOf( 1||2||3||4||5||6||7||8)) {
+    let checkWinner = () => {
+        if(gameBoard.board.turn = 8 && gameBoard.board.indexOf(player.symbol) === winningConditions.indexOf( 1||2||3||4||5||6||7||8)) {
             prompt.textContent = "Player wins!";
             player.score++;
-        } else if (gameBoard.board.indexOf(aiPlayer.symbol) === winningConditions.indexOf( 1||2||3||4||5||6||7||8)) {
-            prompt.textContent =  "AI wins!";
-            aiPlayer.score++;
+        } else if (gameBoard.board.turn = 8 && gameBoard.board.indexOf(computer.symbol) === winningConditions.indexOf( 1||2||3||4||5||6||7||8)) {
+            prompt.textContent =  "computer wins!";
+            computer.score++;
         }
     }
-    return {checkWinner}
-})()
+    return {checkWinner};
+})();
+console.log(game.checkWinner);
+
