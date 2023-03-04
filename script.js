03858acbcc3f786184e5d77cf3ea071176ea720e
Debbie <debby.ek@yahoo.com>
2023-03-03 18:03:17 -0600

    added audio for each symbol

--- script.js
+++ script.js
@@ -18,8 +18,10 @@ let fadeEffect =setInterval(function (){
 popUpStart.addEventListener('click', (fadeOutEffect));
 
 let img = document.querySelector('img');
+let audio = new Audio("images/cursor.mp3");
+let circle = new Audio("images/circle.wav");
+let xAudio = new Audio("images/x.wav");
 img.addEventListener('click', () => {
-    let audio = new Audio("images/cursor.mp3");
     audio.play();
     
 });
@@ -42,14 +44,15 @@ let board = [];
     let turn = 0;
     gameFields.forEach(gameField => gameField.addEventListener('click', initialize) );
     
-    function initialize() {   
+    function initialize() {  
+        
         turn++; 
         let divIndex = this.getAttribute("divIndex");
         if(board[divIndex] = " ") {
             play(this, divIndex);
             checkWinner();
-        } else if(play(this, divIndex) == true){
-            computerPlay(gameField);
+        } else if(board[divIndex] != " "){
+            computerPlay();
             checkWinner();
         }
     };
@@ -60,14 +63,15 @@ let board = [];
         board[index] = `${player.symbol}`;
         gameField.textContent = "X";
         prompt.textContent= " ";
-        
+        xAudio.play(); 
     };
 
     function computerPlay(gameField) {
-        let computerPick = board[Math.floor(Math.random()* gameField.length)];
+        let computerPick = Math.floor(Math.random()* board.length);
         gameField[computerPick].textContent = "O";
         board[computerPick] = `${computer.symbol}`;
         prompt.textContent = "Player's turn";
+        circle.play();
         };
        
 
@@ -85,10 +89,10 @@ let board = [];
     ];
 
     function checkWinner() {
-        if(turn = 8 && board.indexOf(player.symbol) == winningConditions.indexOf( 1||2||3||4||5||6||7||8)) {
+        if(turn == 8 && board.indexOf(`${player.symbol}`) == winningConditions.indexOf( 1||2||3||4||5||6||7||8)) {
             prompt.textContent = "Player wins!";
             player.score++;
-        } else if (turn = 8 && board.indexOf(computer.symbol) == winningConditions.indexOf( 1||2||3||4||5||6||7||8)) {
+        } else if (turn == 8 && board.indexOf(`${computer.symbol}`) == winningConditions.indexOf( 1||2||3||4||5||6||7||8)) {
             prompt.textContent =  "computer wins!";
             computer.score++;
         }
